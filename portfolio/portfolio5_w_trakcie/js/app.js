a6b1cade34756005c1ee42fba9cb44e1d10f7f61
suchydariusz <darek3110@gmail.com>
2019-04-30 16:41:54 +0200

    updated portfolio5

--- portfolio/portfolio5_w_trakcie/js/app.js
+++ portfolio/portfolio5_w_trakcie/js/app.js
@@ -6,19 +6,19 @@ const fliping = () => {
         node: document.getElementById('flip1'),
         from: 0,
         to: 7533,
-        duration: 7 
+        duration: 5 
       });
       new Flip({
         node: document.getElementById('flip2'),
         from: 0,
         to: 4222,
-        duration: 7
+        duration: 5
       });
       new Flip({
         node: document.getElementById('flip3'),
         from: 0,
         to: 6980,
-        duration: 7
+        duration: 5
       });
       new Flip({
         node: document.getElementById('flip4'),
